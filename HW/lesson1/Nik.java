8760d237a85f1c586feba09a37f9647e11f71b31
Songrov <nico@it-nico.de>
2024-02-27 20:04:13 +0100

    update

---
+++ HW/lesson1/Nik.java
@@ -0,0 +1,11 @@
+package prof.HW.lesson1;
+
+import javax.swing.*;
+import java.util.Arrays;
+
+public class Nik {
+    public static void main(String[] args) {
+        Spring age;
+        System.out.println("Hallo Work!");
+    }
+}
